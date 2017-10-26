7e1a8e2fd968849588bd7fb61166369367c4a773
Shabin <d.shabin@gmail.com>
2017-10-26 16:47:46 +0400

    Replacing missing data

---
+++ missing_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#importing dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3]
+print(dataset)
+
+from sklearn.preprocessing import Imputer
+# replacing NaN values with the MEAN of column
+imputer = Imputer(missing_values = 'NaN' , strategy = 'mean' , axis=0)
+imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+print(X)
