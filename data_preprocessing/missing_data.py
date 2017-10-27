bf83b8d81d9ad31cd9bc8456caea7f6be3dae954
Shabin <d.shabin@gmail.com>
2017-10-27 12:21:02 +0400

    feature scaling

--- data_preprocessing/missing_data.py
+++ data_preprocessing/missing_data.py
@@ -17,21 +17,32 @@ print(X)
 from sklearn.preprocessing import LabelEncoder
 labelencoder_X = LabelEncoder()
 X[:,0] = labelencoder_X.fit_transform(X[:,0])
-print(X)
+#print(X)
 
 #create dummy variables
 from sklearn.preprocessing import OneHotEncoder
 #which colum should be encoded
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
-print(X)
+#print(X)
 #encoding YES and NO colum
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
-print(y)
+#print(y)
 
 # Splitting the dataset to Training set and test set
 from sklearn.cross_validation import train_test_split
 X_train , X_test , y_train , y_test = train_test_split(X,y, test_size = 0.2 , random_state = 0 )
-print('__')
-print(X_train,X_test,y_train,y_test)
+#print('__')
+#print(X_train,X_test,y_train,y_test)
+
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+# for test we dont need fit ,
+X_test = sc_X.transform(X_test)
+
+print(X_train)
+print(X_test)
