d74950be3cfffe840dfda674e725a42c82cc87ad
Shabin <d.shabin@gmail.com>
2017-12-17 14:41:53 +0400

    Add requirements.txt - add assoc-rule-learning

---
+++ association_rule_learning/apriori/apriori.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Market_Basket_Optimisation.csv',header = None)
+transactions = []
+for i in range(0,7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0,20)])
+
+#print(transactions)
+
+#Training Apriori on the dataset
+from apyori import apriori
+
+rules = apriori(transactions,min_support = 0.003 , min_confidence = 0.2 , min_lift = 3 , min_lenght = 2)
+
+# Visualising the result
+results = list(rules)
+print(results)
